2df48d2899870c0b6ac1991f26b79ad6325c692d
AntonNV22 <antonnv22@gmail.com>
2020-09-24 14:06:31 +0800

    исправлены замечания

--- src/main/java/service/ServiceCarImpl.java
+++ src/main/java/service/ServiceCarImpl.java
@@ -5,7 +5,7 @@ import model.Car;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ServiceCarImpl implements ServiceCar{
+public class ServiceCarImpl implements ServiceCar {
 
     @Override
     public List<Car> findSome() {
@@ -13,10 +13,10 @@ public class ServiceCarImpl implements ServiceCar{
     }
 
     public List<Car> findThreeCar() {
-        List<Car> cars=new ArrayList<>();
-        String[] models={"BMW","Audi","Mercedes"};
-        String[] colors={"black","White","black"};
-        for(int i=0;i<3;i++){
+        List<Car> cars = new ArrayList<>();
+        String[] models = {"BMW", "Audi", "Mercedes"};
+        String[] colors = {"black", "White", "black"};
+        for (int i = 0; i < 3; i++) {
             cars.add(new Car(i, models[i], colors[i]));
         }
         return cars;
